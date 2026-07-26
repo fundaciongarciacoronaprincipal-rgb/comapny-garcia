59ab05c07d78dd1f3e5c4619d562cf214bdc9a50
fundaciongarciacoronaprincipal-rgb <fundaciongarciacoronaprincipal@gmail.com>
2026-07-26 03:06:24 -0600

    Create anuncios array for video advertisements
    Added an array of video announcements with details.

---
+++ anuncio/videos.js
@@ -0,0 +1,16 @@
+const anuncios = [
+{
+    id: 1,
+    video: "https://miservidor.com/videos/anuncio1.mp4",
+    inicio: "08:00",
+    fin: "18:00",
+    maxReproducciones: 4
+},
+{
+    id: 2,
+    video: "https://miservidor.com/videos/anuncio2.mp4",
+    inicio: "09:00",
+    fin: "22:00",
+    maxReproducciones: 4
+}
+];
