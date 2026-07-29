d8982e5403e5558b0c4a38ccecd56578282d3eb4
fundaciongarciacoronaprincipal-rgb <fundaciongarciacoronaprincipal@gmail.com>
2026-07-28 20:53:13 -0600

    Update timeline.js

--- ggc/timeline.js
+++ ggc/timeline.js
@@ -23,6 +23,14 @@ titulo:"Nacimiento de Corporativo Nexel Digital",
 descripcion:
 "Nace la división especializada en el desarrollo de sistemas web, plataformas digitales y soluciones tecnológicas para empresas e instituciones."
 
+},
+
+
+
+  {
+    fecha: "01 de Mayo de 2026",
+    titulo: "Primer Contrato de Corporativo Nexel Digital",
+    descripcion: "El Director General, J***** Garcia Corona, concreta el primer contrato oficial de Corporativo Nexel Digital con el colegio OFTV NO. 0216 \"Francisco Villa\", marcando el inicio de una nueva etapa de crecimiento y consolidación para el proyecto, el cual posteriormente formaría parte del desarrollo y expansión de GRUPO GARCIA CORONA."
 }
 
 
