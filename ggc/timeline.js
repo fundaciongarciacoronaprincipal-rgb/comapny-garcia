2e0bde7b0ef4ee64ff520b392a925871747a3872
fundaciongarciacoronaprincipal-rgb <fundaciongarciacoronaprincipal@gmail.com>
2026-07-28 20:19:05 -0600

    Add timeline data for GRUPO GARCIA CORONA

---
+++ ggc/timeline.js
@@ -0,0 +1,29 @@
+const timelineData = [
+
+
+{
+
+fecha:"26 de Julio de 2026",
+
+titulo:"Nacimiento de GRUPO GARCIA CORONA",
+
+descripcion:
+"Se establece oficialmente la identidad de GRUPO GARCIA CORONA como un grupo empresarial orientado al desarrollo tecnológico y la creación de nuevos proyectos."
+
+},
+
+
+
+{
+
+fecha:"26 de Julio de 2026",
+
+titulo:"Nacimiento de Corporativo Nexel Digital",
+
+descripcion:
+"Nace la división especializada en el desarrollo de sistemas web, plataformas digitales y soluciones tecnológicas para empresas e instituciones."
+
+}
+
+
+];
